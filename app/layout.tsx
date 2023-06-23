b822608f70b6c31a28c6b941f1c0997a65bf1160
AndreaBorin <andrea.borin.ndr@gmail.com>
2023-06-23 16:24:00 +0200

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,6 @@ const b = Bungee({ weight: "400", subsets: ["latin"] });
 
 export const CHAIN_ID = 338;
 
-export const metadata = {
-  title: "APD | Apocalypse Dudes",
-  description: "AI Cronos NFT Collection by Jack Yolo",
-};
-
 import LayoutClient from "./components/LayoutClient";
 
 export default function RootLayout({
